9b9fbf0ef8ca8085d815ad6da28165b7a1b4bc5b
Erkenoss <xavier.bertin6@gmail.com>
2023-11-30 10:46:06 +0100

    add test_ameni

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,19 +1,49 @@
-#!/usr/bin/python3
-""" """
-from tests.test_models.test_base_model import test_basemodel
+import unittest
+from tests.test_models.test_base_model import TestBaseModel
 from models.amenity import Amenity
 
 
-class test_Amenity(test_basemodel):
-    """ """
+class Test_Amenity():
+    """Test the Amenity class"""
 
-    def __init__(self, *args, **kwargs):
-        """ """
-        super().__init__(*args, **kwargs)
+    def setUp(self):
+        """Set up a clean storage instance before each test."""
+        super().setUp()
         self.name = "Amenity"
         self.value = Amenity
 
-    def test_name2(self):
-        """ """
-        new = self.value()
-        self.assertEqual(type(new.name), str)
+    def test_name_column(self):
+        """Test the name column in the Amenity table."""
+        new_amenity = self.value()
+        self.assertTrue(hasattr(new_amenity, 'name'))
+        self.assertIsInstance(new_amenity.name, str)
+
+    def test_place_amenities_relationship(self):
+        """Test the relationship between Amenity and Place."""
+        amenity = Amenity(name="Gym")
+        self.assertIsInstance(amenity.place_amenities, list)
+
+    def test_save_to_database(self):
+        """Test saving an Amenity instance to the database."""
+        amenity = Amenity(name="Pool")
+        self.save_instance(amenity)
+
+        amenity_key = "Amenity." + amenity.id
+        self.assertTrue(amenity_key in self.storage.all(Amenity))
+
+    def test_reload_from_database(self):
+        """Test reloading an Amenity instance from the database."""
+        amenity = Amenity(name="Sauna")
+        self.save_instance(amenity)
+
+        amenity_id = amenity.id
+        self.storage.reload()
+
+        # Test that the reloaded object has the same attributes
+        reloaded_amenity = self.storage.get(Amenity, amenity_id)
+        self.assertIsNotNone(reloaded_amenity)
+        self.assertEqual(amenity.name, reloaded_amenity.name)
+
+
+if __name__ == "__main__":
+    unittest.main()
