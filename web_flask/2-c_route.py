01f42ce3329c1a3ad8358a8a98baffa47b03118a
Letho1972 <ericmarescq@gmail.com>
2023-12-11 14:16:21 +0100

    Display “C ” followed by the value whithout _

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+""" Write a script that starts a Flask web application """
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def Hello_HBNB():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def HBNB():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def Variable(text):
+    text = text.replace('_', ' ')
+    return f"C {text}"
+
+
+if __name__ == '__main__':
+
+    """ Run the Flask app on 0.0.0.0:5000 """
+    app.run(host='0.0.0.0', port=5000)
