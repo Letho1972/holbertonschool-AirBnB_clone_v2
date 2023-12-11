15136d2b4049fc4b5384b960046c764bebb4b921
Letho1972 <ericmarescq@gmail.com>
2023-12-11 15:48:13 +0100

    Is it a number?

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+""" Write a script that starts a Flask web application """
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def Hello_HBNB():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def HBNB():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def Variable(text):
+    text = text.replace('_', ' ')
+    return f"C {text}"
+
+
+@app.route("/python/", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def Python(text="is cool"):
+    text = text.replace('_', ' ')
+    return f"Python {text}"
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def is_int(n):
+    if isinstance(n, int):
+        return f"{n} is a number"
+
+
+if __name__ == '__main__':
+
+    """ Run the Flask app on 0.0.0.0:5000 """
+    app.run(host='0.0.0.0', port=5000)
